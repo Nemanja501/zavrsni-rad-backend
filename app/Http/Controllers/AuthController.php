e56979da30ba1d8efd0012d3dbeecd2abaf9aee7
Nemanja501 <nemanjavulin5@gmail.com>
2024-03-01 17:44:35 +0100

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\AuthResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -18,8 +20,36 @@ class AuthController extends Controller
             'email' => $validated['email'],
             'password' => Hash::make($validated['password'])
         ]);
+        $token = $user->createToken($user->first_name . $user->last_name . 'token')->plainTextToken;
 
-        return new AuthResource($user);
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(LoginRequest $request){
+        $validated = $request->validated();
+        $user = User::where('email', $validated['email'])->first();
+        if(!$user || !Hash::check($validated['password'], $user->password)){
+            return response()->json([
+                'error' => 'Invalid credentials' 
+            ]);
+        }
+        $token = $user->createToken($user->first_name . $user->last_name . 'token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function logout(){
+        Auth::user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out.'
+        ]);
     }
 
     
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
